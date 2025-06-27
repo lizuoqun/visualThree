96c2716aef1a6c8e99e187713e3de4b5c221bced
modify <2645929971@qq.com>
2025-06-27 15:21:15 +0800

    feat(shader): 添加矩阵和砖块效果的 GLSL 着色器
    - 新增 09-matrix.glsl 文件，实现矩阵效果
    - 新增 09-brick.glsl 文件，实现砖块效果
    - 着色器代码包含旋转、平铺和盒形等实用函数

---
+++ shader/glsl/09-brick.glsl
@@ -0,0 +1,38 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+// 放大倍数、再缩小
+vec2 brickTile(vec2 _st, float _zoom){
+    _st *= _zoom;
+    _st.x += step(1., mod(_st.y, 2.0)) * 0.5;
+    return fract(_st);
+}
+
+float box(vec2 _st, vec2 _size){
+    _size = vec2(0.5)-_size*0.5;
+    vec2 uv = smoothstep(_size, _size+vec2(1e-4), _st);
+    uv *= smoothstep(_size, _size+vec2(1e-4), vec2(1.0)-_st);
+    return uv.x*uv.y;
+}
+
+void main(void){
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec3 color = vec3(0.0);
+
+
+    // http://www.jaharrison.me.uk/Brickwork/Sizes.html
+
+    st /= vec2(2.15, 0.65)/1.5;
+
+    st = brickTile(st, 10.0);
+
+    color = vec3(box(st, vec2(0.95)));
+
+    //    color = vec3(st, 0.0);
+
+    gl_FragColor = vec4(color, 1.0);
+}
