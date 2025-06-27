0f70faac14d52ea70abfc46a10987fa1bd6d4628
modify <2645929971@qq.com>
2025-06-27 14:58:56 +0800

    feat(shader): 添加新的 GLSL着色器示例
    - 新增 08-rotate.glsl、08-scale.glsl 和 08-translate.glsl 三个着色器示例
    - 修改 07-integration.glsl，简化代码并绘制八边形
    - 新增的着色器示例展示了如何实现旋转、缩放和平移效果
    - 优化了代码格式和注释，提高了可读性

---
+++ shader/glsl/09-pattern.glsl
@@ -0,0 +1,25 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float circle(in vec2 _st, in float _radius){
+    vec2 l = _st-vec2(0.5);
+    return 1.0 - smoothstep(_radius * 0.99, _radius * 1.01, dot(l, l) * 10.0);;
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+    vec3 color = vec3(0.0);
+
+    // 先给他放大五倍、在缩小在0-1之间
+    st *= 5.0;
+    st = fract(st);
+
+    color = vec3(st, 0.0);
+    // color = vec3(circle(st,0.5));
+
+    gl_FragColor = vec4(color, 1.0);
+}
